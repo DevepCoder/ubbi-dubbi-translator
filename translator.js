6e75c3d3e5de1174a552cc4bb7e6fdfee0f5f162
DevepCoder <53006913+DevepCoder@users.noreply.github.com>
2021-03-05 21:32:31 +0200

    Create translator.js

---
+++ translator.js
@@ -0,0 +1,14 @@
+function translator(e) {
+    this.translate = () => {
+        const t = ["a", "e", "i", "o", "u"];
+        let n = e.length;
+        return e
+            .split("").map((e, l) => {
+                let r = e.toLowerCase(),
+                    s = e === r ? "ub" : "UB";
+                return "e" === r && l + 1 === n ? e : t.includes(r) ? s + e : e;
+            })
+            .join("");
+    }
+}
+export default translator;
